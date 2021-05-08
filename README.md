82acc18a1c291b6b6d814e4569721872b1c98ddc
Hakim Mektoub <hakim.mektoub@polymtl.ca>
2021-05-08 15:17:16 -0400

    Works on bein1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # noAdBlockMessageFootball
+Really basic script that changes id of the displayed message to hide the stream. 
+
+Unfortunately it doesn't work if  there is many nested iframes.
+
+Feel free to fork and improve
--- content.js
+++ content.js
@@ -1,2 +1,21 @@
-var element = document.getElementById("tc7e");
-element.id = "notTc7e";
+let observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        if (!mutation.addedNodes.length) return
+        for (let i = 0; i < mutation.addedNodes.length; i++) {
+            // do things to your newly added nodes here
+            let node = mutation.addedNodes[i]
+            if (node == document.getElementById("tc7e")) {
+                console.log("here");
+                node.id = "notTc7e";
+                observer.disconnect()
+            }
+        }
+    })
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    characterData: true
+});
